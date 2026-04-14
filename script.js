35c050eaa3a3c660dbf3e966ac70479515b9cc7d
1301307105-star <1301307105@s.tusd1.org>
2026-04-14 15:45:29 +0000

    not done

--- script.js
+++ script.js
@@ -8,19 +8,26 @@ function processForm() {
     console.log("click");
     // TODO: prevent this function from reloading the page when the form is submitted
 
-    // TODO: Create a newUser object that has all the user's info from the form
-
+    // Create a newUser object that has all the user's info from the form
+    let newUser = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        birthdate: document.getElementById("birthdate").value,
+        favColor: document.getElementById("favColor").value
+    }
     // TODO: Call the addUser function and pass the newUser object as a parameter
+
 }
 
 function addUser(user) {
     // Create a div to hold the user's info
     let div = document.createElement("div");
     // TODO: Set the background color of the div to the user's favorite color
-
+    div.style.backgroundColor = user.favColor
     // Create a heading for the user's name
     let name = document.createElement("h2");
     // TODO: Set the text using the object data
+    name.innerText = user.name;
     div.appendChild(name); // Add the heading to the div
 
     // Create a paragraph for the user's email
